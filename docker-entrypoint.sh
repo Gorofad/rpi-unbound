5b5fcb6e5ca26440cd0c722823def64cba33c796
Gorofad <gorofad@posteo.de>
2017-10-13 02:27:08 +0200

    Fixed DNSSEC Validation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,14 @@
 
 set -ex
 
+chown -R _unbound:_unbound /data/unbound/
+
+echo ">> Load Anchor file"
+unbound-anchor -v -r "/data/unbound/root.hints"
+cp /usr/share/dnssec-root/trusted-key.key /data/unbound/root.key
+
 echo ">> Check Unbound config files"
 unbound-checkconf
 
-echo ">> Load Anchor file"
-unbound-anchor -v
-
 echo ">> Start Unbound"
-unbound
+unbound -c /data/unbound/unbound.conf
