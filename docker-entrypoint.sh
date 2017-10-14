128145bc4de5a61232431c2952d9f5e5603fd629
Gorofad <gorofad@posteo.de>
2017-10-14 16:58:30 +0200

    small fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ unbound-anchor -v -r "/data/unbound/root.hints"
 cp /usr/share/dnssec-root/trusted-key.key /data/unbound/root.key
 
 echo ">> Check Unbound config files"
-unbound-checkconf
+unbound-checkconf /data/unbound/unbound.conf
 
 echo ">> Start Unbound"
 unbound -c /data/unbound/unbound.conf
